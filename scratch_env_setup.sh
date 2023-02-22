f49c38a4b13ac3574db575c64b561eab5552fe11
thirupathireddy665 <emireddy@ualberta.ca>
2023-02-22 16:52:06 -0700

    add numpy to env

--- scratch_env_setup.sh
+++ scratch_env_setup.sh
@@ -5,6 +5,7 @@ cd ~/scratch/
 rm -rf crossbeam_env
 
 module load python/3.8
+module load scipy-stack/2020b
 virtualenv --no-download ./crossbeam_env
 source ./crossbeam_env/bin/activate
 pip install --no-index --upgrade pip
@@ -12,3 +13,4 @@ pip install torch==1.10.2+cu113 -f https://download.pytorch.org/whl/cu113/torch_
 pip install torch-scatter -f https://data.pyg.org/whl/torch-1.10.0+cu113.html
 pip install absl-py
 pip install tqdm
+pip install pickle5
