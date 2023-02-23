63ee5cb4dfa74ce58daa7c9a820ee66b3acb4227
thirupathireddy665 <emireddy@ualberta.ca>
2023-02-22 18:54:43 -0700

    training time update

--- crossbeam/experiment/bustle/bustle_main_dist.sh
+++ crossbeam/experiment/bustle/bustle_main_dist.sh
@@ -3,7 +3,7 @@
 #SBATCH --gres=gpu:1
 #SBATCH --cpus-per-task=1   # maximum CPU cores per GPU request: 6 on Cedar, 16 on Graham.
 #SBATCH --mem=64000M        # memory per node
-#SBATCH --time=10:00:00     # time of the task
+#SBATCH --time=03:00:00     # time of the task
 #SBATCH --account=def-lelis
 #SBATCH --output=%N-%j.out
 #SBATCH --mail-user=emireddy@ualberta.ca
